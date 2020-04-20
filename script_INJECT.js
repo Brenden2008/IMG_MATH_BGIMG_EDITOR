e1e39a398f6ff3f64df904a16db9b2d7445f468c
Brenden2008 <42918416+Brenden2008@users.noreply.github.com>
2020-04-20 14:40:55 -0500

    Update script_INJECT.js

--- script_INJECT.js
+++ script_INJECT.js
@@ -1,1 +1,2 @@
-document.getElementById("main-content").style.backgroundImage = "url(https://media.giphy.com/media/xT4uQF7h39mlsF5czK/giphy.gif)";
+var url = prompt("Please enter the link to the image", "https://www.testsite.com/tree.jpeg");
+document.getElementById("main-content").style.backgroundImage = "url(url)";
