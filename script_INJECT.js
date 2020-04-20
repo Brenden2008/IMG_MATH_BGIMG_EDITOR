9c4aedc03d6f07aa1e29c112d5c2bd4c7c0eec58
Brenden2008 <42918416+Brenden2008@users.noreply.github.com>
2020-04-20 14:58:12 -0500

    Update script_INJECT.js

--- script_INJECT.js
+++ script_INJECT.js
@@ -1,2 +1,6 @@
-var url = prompt("Please enter the link to the image", "https://www.testsite.com/tree.jpeg");
+var Ask_url = prompt("Please enter the link to the image (.gif accepted)", "https://www.testsite.com/tree.jpeg");
+sessionStorage.setItem("Url", Ask_url);
+var url = sessionStorage.getItem("Url");
+
+
 document.getElementById("main-content").style.backgroundImage = "url(url)";
