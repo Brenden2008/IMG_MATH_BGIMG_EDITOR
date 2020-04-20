24b48f7e36752970b0760522bf74ea1c8e9cbabf
Brenden2008 <42918416+Brenden2008@users.noreply.github.com>
2020-04-20 15:05:46 -0500

    Update script_INJECT.js

--- script_INJECT.js
+++ script_INJECT.js
@@ -1,6 +1,10 @@
-var Ask_url = prompt("Please enter the link to the image (.gif accepted)", "https://www.testsite.com/tree.jpeg");
-sessionStorage.setItem("Url", Ask_url);
-var url = sessionStorage.getItem("Url");
-
-
-document.getElementById("main-content").style.backgroundImage = "url(url)";
+ if (confirm("Press OK to change the url. Press Cancel to apply the current url.")) {
+    
+  var Ask_url = prompt("Please enter the link to the image (.gif accepted)", "https://www.testsite.com/tree.jpeg");
+  sessionStorage.setItem("Url", Ask_url);
+  var url = sessionStorage.getItem("Url");
+  document.getElementById("main-content").style.backgroundImage = "url(url)"; 
+   
+  } else {
+   document.getElementById("main-content").style.backgroundImage = "url(url)";
+  }
